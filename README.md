7f3924317d8bf7a1c7f3cae1858e22313bbdbbce
AliangTech <seminapater@gmail.com>
2023-08-19 11:27:41 +0800

    🐛

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # env-install
 
-
 # 未使用sh脚本有权限 先执行chmod +x install.sh
 
-# go 出一键安装命令
+# 有时候volta 下载链接会失败 可以进入pkg 文件夹执行 chmod +x ./volta.sh 在执行./volta.sh
 
-sudo apt install golang-go
-sudo apt install gccgo-go 
+# 安装go版本 可以手动修改一下版本号就行在go.sh
--- pkg/volta.sh
+++ pkg/volta.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # node版本管理工具安装
 curl -fsSL https://get.volta.sh | bash
+# 等待 volta 生效
+while ! command -v volta &> /dev/null; do
+    echo "等待 volta 生效..."
+    sleep 1
+done
 echo "volta----安装完成"
 volta install node
 echo "node----安装完成"
